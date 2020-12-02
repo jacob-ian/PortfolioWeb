f0bb1b8f8c784e6c097087029f5d4bf17a7426c1
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-12-02 10:50:03 +0800

    chore(ENV): Add firebase config to environment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyAk0KMrouGcn7C89Xw7BNIVWzLzufxRxzY",
+    authDomain: "jacobianmatthews-portfolio.firebaseapp.com",
+    databaseURL: "https://jacobianmatthews-portfolio.firebaseio.com",
+    projectId: "jacobianmatthews-portfolio",
+    storageBucket: "jacobianmatthews-portfolio.appspot.com",
+    messagingSenderId: "1059888186279",
+    appId: "1:1059888186279:web:c78555ee8c3875b3c66c82",
+    measurementId: "G-8WN7W0MXYX"
+  }
 };
 
 /*
