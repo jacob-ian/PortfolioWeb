1bb9bb80b616b537e771d367713550c5d8212942
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-12-18 10:29:41 +0800

    fix(Register): Various changes
    - Add an image to indicate photo editing
    - Remove query parameter and user subscription

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../core/services/auth.service';
 import { User } from '@functions/users/users.models';
 import { Observable, Subscription } from 'rxjs';
-import { switchMap, take } from 'rxjs/operators';
+import { first, switchMap, take, tap } from 'rxjs/operators';
 import { NotificationsService } from '../core/notifications/notifications.service';
 
 @Component({
@@ -20,13 +20,8 @@ export class RegisterComponent implements OnInit, OnDestroy {
     notificationsSubscribe: [true, Validators.required],
   });
 
-  // The subscription to the route's query params
-  querySub: Subscription;
+  // The subscription to the registered state
   registeredSub: Subscription;
-
-  // The redirect address
-  redirectPath: string;
-  redirectParams: any;
 
   // The user observable
   user$: Observable<User>;
@@ -44,37 +39,8 @@ export class RegisterComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    // Get the query params to check for a uid
-    this.querySub = this.route.queryParams.subscribe((params) => {
-      // Check if the uid param exists
-      if (!params.uid) {
-        // We can redirect back to the login page
-        return this.router.navigate(['/login']);
-      }
-
-      // Check if there is a redirect path or params
-      this.redirectPath = params.redirect_path
-        ? params.redirect_path
-        : undefined;
-      this.redirectParams = params.redirect_params
-        ? params.redirect_params
-        : undefined;
-    });
-
     // Get the user observable
     this.user$ = this.auth.user$;
-
-    // Create the subscription to the user to check if registration is required
-    this.registeredSub = this.user$.pipe(take(1)).subscribe((user) => {
-      // Check if there is a user
-      if (user) {
-        // Check if the user has the is registered boolean
-        if (user.isRegistered) {
-          // We can redirect the user away from here
-          this.redirectUser();
-        }
-      }
-    });
   }
 
   /**
@@ -125,9 +91,18 @@ export class RegisterComponent implements OnInit, OnDestroy {
       .update({
         notifications: { email: emailSubscribe },
       })
-      .then(() => {
-        // Redirect the user back to the homepage
-        return this.redirectUser();
+      .then(async () => {
+        // Fetch the query parameters for the page
+        const params = await this.route.queryParams.pipe(first()).toPromise();
+
+        // Check for redirect instructions
+        if (params.redirect_path) {
+          // Redirect to the path
+          this.router.navigate([params.redirect_path]);
+          return;
+        } else {
+          this.router.navigate(['/dashboard']);
+        }
       })
       .catch((err) => {
         // Set the error
@@ -135,35 +110,8 @@ export class RegisterComponent implements OnInit, OnDestroy {
       });
   }
 
-  /**
-   * Redirects the user either to the dashboard or to the previous page they were on.
-   * @returns void
-   */
-  redirectUser(): void {
-    // Check if there is a redirect path
-    if (this.redirectPath) {
-      // Check for params
-      if (this.redirectParams) {
-        // Redirect to the address with the params
-        this.router.navigate([this.redirectPath], {
-          queryParams: this.redirectParams,
-        });
-      } else {
-        // Redirect to the address without params
-        this.router.navigate([this.redirectPath]);
-      }
-    } else {
-      // Redirect the user to the dashboard
-      this.router.navigate(['/dashboard']);
-    }
-  }
-
   ngOnDestroy(): void {
-    // Check for the query subscription and unsubscribe
-    if (this.querySub) {
-      this.querySub.unsubscribe();
-    }
-
+    // Unsubscribe from the subscriptions
     if (this.registeredSub) {
       this.registeredSub.unsubscribe();
     }
