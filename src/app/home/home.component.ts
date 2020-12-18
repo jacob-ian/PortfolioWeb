f806d66026e0f1409d9337d9b78893f9ae5f389a
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-12-18 10:30:02 +0800

    style(Register): Create pseudo-element for images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationsService } from '../core/notifications/notifications.service';
 import { AuthService } from '../core/services/auth.service';
 
 @Component({
@@ -7,7 +8,28 @@ import { AuthService } from '../core/services/auth.service';
   styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    public notifications: NotificationsService
+  ) {}
 
   ngOnInit(): void {}
+
+  testNotification() {
+    // Create a test notification
+    this.notifications.createNotification({
+      notification: {
+        title: 'Test Notification',
+        body: 'This is a big ole test notification with some random content.',
+        icon: '/assets/icons/icon-512x512.png',
+      },
+      data: {
+        routerLink: '/blog',
+      },
+    });
+  }
+
+  async signOut(): Promise<void> {
+    return await this.auth.signOut();
+  }
 }
