e5359dd8d4cf511e151ba488f7cd1655655ccb63
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-03-06 15:38:10 +0800

    feat(*): Add Privacy Policy and Terms components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'privacy', component: PrivacyComponent },
+  { path: 'terms', component: TermsComponent },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '404' },
 ];
