cb4cf4b86fad1babf3582a31fc78f86bc3a7008a
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-03-11 16:43:04 +0800

    feat(*): Connect MetadataService to the App
    - Enable the meta tag updating service to run

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MetadataService } from './services/meta/metadata.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'PortfolioWeb';
+
+  constructor(private metaDataService: MetadataService) {}
 }
