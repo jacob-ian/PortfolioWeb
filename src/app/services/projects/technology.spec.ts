1aa09f6ee9ff986e9c0cede7f8e513c85e25b463
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-04-01 15:47:09 +0800

    test(Projects): Write unit tests for Technology

--- src/app/services/projects/technology.spec.ts
+++ src/app/services/projects/technology.spec.ts
@@ -1,9 +1,28 @@
-import { Technology } from './technology';
+import { MockFirestore } from '../database/mock-firestore';
+import { Technology, TechnologyDocument } from './technology';
+
+const TEST_TECH_1: TechnologyDocument = {
+  id: '1',
+  name: 'angular',
+};
 
 describe('Technology', () => {
-  let mockFirestore: any;
+  let mockFirestore: any = new MockFirestore([]);
+  let technology: Technology;
 
   describe('Test getId()', () => {
-    it('Should return ');
+    it('Should return an ID with document instantiation', () => {
+      technology = new Technology(mockFirestore, TEST_TECH_1);
+      let id = technology.getId();
+      expect(id).toBe(TEST_TECH_1.id);
+    });
+  });
+
+  describe('Test getName()', () => {
+    it('Should return the name with doc instantiation', () => {
+      technology = new Technology(mockFirestore, TEST_TECH_1);
+      let name = technology.getName();
+      expect(name).toBe(TEST_TECH_1.name);
+    });
   });
 });
