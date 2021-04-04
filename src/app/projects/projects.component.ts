684b7a675eb19ea0363638cc495aa7f9240d4a58
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-04-04 22:06:59 +0800

    fix(Projects): Fix Page Loading Bug
    - Remove usage of combineLatest
    - Create projects, filter, filteredProjects behavior subjects
    - Use subscriptions to update filteredProjects on change
    - Note: This is a workaround

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { Project } from '../services/projects/project';
 import { ProjectService } from '../services/projects/project.service';
 
@@ -9,73 +8,99 @@ import { ProjectService } from '../services/projects/project.service';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.sass'],
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, OnDestroy {
   private filter: BehaviorSubject<string[]>;
+  private projects: BehaviorSubject<Project[]>;
+  private filteredProjects: BehaviorSubject<Project[]>;
+
+  private projectSubscription: Subscription;
+  private filterSubscription: Subscription;
 
   constructor(private projectService: ProjectService) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.filter = new BehaviorSubject([]);
+    this.projects = new BehaviorSubject([]);
+    this.filteredProjects = new BehaviorSubject([]);
+    this.startFilteringProjects();
   }
 
-  public onFilter(filter: string[]): void {
-    this.filter.next(filter);
+  /**
+   * This method was used as a work around due to RXJS combineLatest
+   * not working with Angular Universal and causing infinite page loading
+   * with no component render. The error leads to a JavaScript heap error on the
+   * Universal Express server.
+   */
+  private startFilteringProjects(): void {
+    this.projectSubscription = this.subscribeToProjects();
+    this.filterSubscription = this.subscribeToFilter();
   }
 
-  public getFilteredProjects(): Observable<Project[]> {
-    let combinedObservable = this.createFilteredProjectsObservable();
-    return combinedObservable;
-  }
-
-  private createFilteredProjectsObservable(): Observable<Project[]> {
-    return combineLatest([
-      this.getProjectsObservable(),
-      this.getFilterObservable(),
-    ]).pipe(
-      filter(([projects, technologies]) => !!projects && !!technologies),
-      map(([projects, technologies]) =>
-        this.mergeProjectsTechnologies(projects, technologies)
-      )
-    );
+  private subscribeToProjects(): Subscription {
+    return this.getProjectsObservable().subscribe((projects) => {
+      this.projects.next(projects);
+      this.onObservableChange();
+    });
   }
 
   private getProjectsObservable(): Observable<Project[]> {
     return this.projectService.getProjects();
   }
 
+  private onObservableChange(): void {
+    let filteredProjects = this.filterProjectsWithTechnologies();
+    return this.filteredProjects.next(filteredProjects);
+  }
+
+  private filterProjectsWithTechnologies(): Project[] {
+    let projects = this.projects.value;
+    let technologies = this.filter.value;
+    if (this.cantFilterProjects()) {
+      return projects;
+    }
+    return projects.filter((project) => project.usesTechnologies(technologies));
+  }
+
+  private cantFilterProjects(): boolean {
+    return this.noProjects() || this.noTechnologies();
+  }
+
+  private noProjects(): boolean {
+    return this.projects.value.length === 0;
+  }
+
+  private noTechnologies(): boolean {
+    return this.filter.value.length === 0;
+  }
+
+  private subscribeToFilter(): Subscription {
+    return this.getFilterObservable().subscribe(() =>
+      this.onObservableChange()
+    );
+  }
+
   private getFilterObservable(): Observable<string[]> {
     return this.filter.asObservable();
   }
 
-  private mergeProjectsTechnologies(
-    projects: Project[],
-    technologies: string[]
-  ): Project[] {
-    if (this.noProjects(projects) || this.noTechnologies(technologies)) {
-      return projects;
+  public getFilteredProjects(): Observable<Project[]> {
+    return this.filteredProjects.asObservable();
+  }
+
+  public onFilter(filter: string[]): void {
+    this.filter.next(filter);
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribeFromObservables();
+  }
+
+  private unsubscribeFromObservables(): void {
+    if (this.filterSubscription) {
+      this.filterSubscription.unsubscribe();
     }
-
-    return this.filterProjectsWithTechnologies(projects, technologies);
-  }
-
-  private noProjects(projects: Project[]) {
-    return projects.length === 0;
-  }
-
-  private noTechnologies(technologies: string[]) {
-    return technologies.length === 0;
-  }
-
-  private filterProjectsWithTechnologies(
-    projects: Project[],
-    technologies: string[]
-  ): Project[] {
-    let filteredProjects = [];
-    projects.forEach((project) => {
-      if (project.usesTechnologies(technologies)) {
-        filteredProjects.push(project);
-      }
-    });
-    return filteredProjects;
+    if (this.projectSubscription) {
+      this.projectSubscription.unsubscribe();
+    }
   }
 }
