b4ca4559ab5481b3d889d762a89c643392e07b7c
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-04-07 11:46:40 +0800

    fix(Functions): Move into Firebase Functions
    - Move dist and output directories to Firebase Functions directory

---
+++ functions/dist/PortfolioWeb/server/firebase-analytics.js
@@ -0,0 +1,22 @@
+exports.ids = ["firebase-analytics"];
+exports.modules = {
+
+/***/ "i3Uh":
+/*!***********************************************************!*\
+  !*** ./node_modules/firebase/analytics/dist/index.cjs.js ***!
+  \***********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(/*! @firebase/analytics */ "GW2D");
+
+//# sourceMappingURL=index.cjs.js.map
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=firebase-analytics.js.map
---
+++ functions/dist/PortfolioWeb/server/firebase-auth.js
@@ -0,0 +1,22 @@
+exports.ids = ["firebase-auth"];
+exports.modules = {
+
+/***/ "CMMD":
+/*!******************************************************!*\
+  !*** ./node_modules/firebase/auth/dist/index.cjs.js ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(/*! @firebase/auth */ "LLvW");
+
+//# sourceMappingURL=index.cjs.js.map
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=firebase-auth.js.map
