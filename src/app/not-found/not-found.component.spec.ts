8baeec2fba528a47e8da95024843852a3bdac7b0
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-04-07 11:15:29 +0800

    refactor(404): Update Tests

--- src/app/not-found/not-found.component.spec.ts
+++ src/app/not-found/not-found.component.spec.ts
@@ -1,7 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute, Router } from '@angular/router';
-
+import { ActivatedRoute } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
 import { By } from '@angular/platform-browser';
 import { take } from 'rxjs/operators';
