17940f136723d33eb38605a964753c74c3a1925d
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-04-16 18:14:09 +0800

    fix(Functions): Move initialise app to index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
 import { FunctionParser } from 'firebase-backend';
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 const { app } = require('./server/main');
+
+admin.initializeApp();
 
 exports = new FunctionParser(__dirname, exports).exports;
 exports.ssr = functions.https.onRequest(app());
