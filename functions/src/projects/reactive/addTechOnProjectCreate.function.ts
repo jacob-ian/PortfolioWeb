5faa965c6b8d2d6aec26007ebd430fc36df5c1e4
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-04-16 12:57:39 +0800

    feat(Functions): Update addTechOnProjectCreate
    - Add method to add project ID to Technology document array on create

--- functions/src/projects/reactive/addTechOnProjectCreate.function.ts
+++ functions/src/projects/reactive/addTechOnProjectCreate.function.ts
@@ -12,48 +12,91 @@ export const addTechOnProjectCreate = functions.firestore
   .document('projects/{projectId}')
   .onCreate((snap) => {
     let project = <Projects.Document>snap.data();
+    let projectId = project.id;
     let technologies = project.technologies;
-    return updateTechnologiesCollection(technologies);
+    return updateTechnologiesCollection(projectId, technologies);
   });
 
 async function updateTechnologiesCollection(
+  projectId: string,
   technologies: string[]
 ): Promise<void> {
   try {
     technologies.forEach(
-      async (technology) => await upsertTechnology(technology)
+      async (technology) => await upsertTechnology(technology, projectId)
     );
   } catch (error) {
     console.error(error);
   }
 }
 
-async function upsertTechnology(technology: string): Promise<void> {
-  if (await technologyExists(technology)) {
-    return;
-  }
-  return await addTechnology(technology);
-}
-
-async function technologyExists(technology: string): Promise<boolean> {
-  let docs = await queryTechnologies(technology);
-  let exists = !docs.empty;
-  return exists;
-}
-
-async function queryTechnologies(
-  technology: string
-): Promise<DocumentCollection> {
+async function upsertTechnology(
+  technology: string,
+  projectId: string
+): Promise<void> {
   try {
-    return await technologiesCollection.where('name', '==', technology).get();
+    return await updateTechnologyWithProjectId(technology, projectId);
+  } catch (error) {
+    let code = error.error;
+    if (code === 'not-found') {
+      return await createTechnology(technology, projectId);
+    }
+    throw error;
+  }
+}
+
+async function updateTechnologyWithProjectId(
+  technology: string,
+  projectId: string
+): Promise<void> {
+  let query = await queryTechnologies(technology);
+
+  if (query.empty) {
+    throw {
+      error: 'not-found',
+      message: `Technology "${technology}" doesn't exist.`,
+    };
+  }
+
+  if (query.size > 1) {
+    throw {
+      error: 'internal',
+      message: `More than one technology "${technology}" document exists.`,
+    };
+  }
+
+  let technologyId = query.docs[0].id;
+  return await addProjectToTechnology(technologyId, projectId);
+}
+
+async function queryTechnologies(name: string): Promise<DocumentCollection> {
+  try {
+    return await technologiesCollection.where('name', '==', name).get();
   } catch (error) {
     throw error;
   }
 }
 
-async function addTechnology(technology: string): Promise<void> {
+async function addProjectToTechnology(
+  technologyId: string,
+  projectId: string
+): Promise<void> {
+  try {
+    let document = technologiesCollection.doc(technologyId);
+    await document.update({
+      projects: admin.firestore.FieldValue.arrayUnion(projectId),
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTechnology(
+  technology: string,
+  projectId: string
+): Promise<void> {
   let id = technologiesCollection.doc().id;
-  return await saveToFirestore({ id, name: technology });
+  return await saveToFirestore({ id, name: technology, projects: [projectId] });
 }
 
 async function saveToFirestore(document: Technologies.Document): Promise<void> {
